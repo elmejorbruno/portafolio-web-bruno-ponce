b9710570ec98eda9835fa8cc9e67564a321fcb59
elmejorbruno <brunoponceprop@gmail.com>
2025-08-13 16:59:15 -0400

    Se agrego los estilos y estrutura al archivos /AboutMeMain.jsx

--- src/components/aboutMeSection/AboutMeMain.jsx
+++ src/components/aboutMeSection/AboutMeMain.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components"
-import AboutMeImage from "./AboutMeImage";
+// import AboutMeImage from "./AboutMeImage";
 import AboutMeText from "./AboutMeText";
 
 const AboutMeMain = () => {
   return (
     <ContainerAbout>
+      <ContentAbout>
         <AboutMeText/>
-        <AboutMeImage/>
+
+      </ContentAbout>
+        {/* <AboutMeImage/> */}
     </ContainerAbout>
   )
 }
@@ -15,23 +18,32 @@ export default AboutMeMain
 
 const ContainerAbout = styled.div`
   display: flex;
-  justify-content:space-between;
+  justify-content:center;
   gap: 12px;
   max-width: 1200px;
+  padding: 30px;
+  text-align: justify;
+ 
   @media (min-width: 768px) {
     flex-direction: row;
     
   }
-  	@media (max-width: 648px) { 
-      flex-direction: column; 
-    }
+  @media (max-width: 648px) { 
+    flex-direction: column; 
+  }
   margin-left: auto;
   margin-right: auto;
   /* gap-12
   px-4 */
   /* max-w[1200px]
- 
+  
   mt-[100px] */
   
   align-items: center;
-`;
+  `;
+  
+  const ContentAbout = styled.div`
+    width: 70%;
+   font-size: 1.2em;
+   line-height: 1.5;
+  `;
